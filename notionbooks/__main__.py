9879c785498fb03c8df253562fce72b43dc160a8
Antonio Feregrino <antonio.feregrino@gmail.com>
2023-03-19 04:48:10 +0000

    Improve format

--- notionbooks/__main__.py
+++ notionbooks/__main__.py
@@ -21,6 +21,15 @@ pages = notion_client.get_pages(DATABASE_ID, "Status", "Review")
 
 OUTPUT.mkdir(exist_ok=True)
 
+
+mapping = {
+    "bold": "b",
+    "italic": "i",
+    "strikethrough": "s",
+    "underline": "u",
+    "code": "code",
+}
+
 def create_notebook_from_blocks(blocks, title):
     nb = new_notebook()
     for block in blocks:
@@ -32,24 +41,33 @@ def create_notebook_from_blocks(blocks, title):
                 nb.cells.append(new_code_cell(actual_code))
         else:
             print(block_type)
+            level = -1
+            if block_type.startswith('heading'):
+                level = int(block_type[-1])
+
             contents = block[block_type].get("text", block[block_type].get("rich_text", []))
             paragraph_content_tags = []
             for content in contents:
                 if text := content.get("text"):
                     text_content = text["content"]
                     annotations = content["annotations"]
-                    annotations_tags = ["bold", "italic", "strikethrough", "underline", "code"]
                     annotations = {
-                        annotation:annotations.get(annotation, False) for annotation in annotations_tags
+                        annotation:annotations.get(annotation, False) for annotation in mapping.keys()
                     }
+                    for annotation, tag in mapping.items():
+                        if annotations.get(annotation):
+                            text_content = f"<{tag}>{text_content}</{tag}>"
+                    if level > 0:
+                        text_content = f"{'#' * level} {text_content}"
+                        level = -1
                     paragraph_content_tags.append(text_content)
                 elif equation := content.get("equation"):
                     paragraph_content_tags.append(f'${equation["expression"]}$')
             paragraph_content = " ".join(paragraph_content_tags)
             nb.cells.append(new_markdown_cell(paragraph_content))
-            pass
 
     nbformat.write(nb, OUTPUT/f"{title}.ipynb")
+
 
 
 for page in pages:
